d145e2c880236f8656aded9b6e6d21b34f5db3e6
lewis617 <897473127@qq.com>
2016-11-04 16:00:07 +0800

    remove jac and use flask-assets

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # flask-seed
+
+### 运行方法：
+
+```sh
+python app.py runserver -h 0.0.0.0 -p 8080
+```
+
+> 生产环境下，需要将环境变量 ENV_MODE 设为 PROD，来禁用 DEBUG 模式。
+
+### 构建方法
+
+```sh
+python app.py assets build  --parse-templates
+python app.py assets clean  --parse-templates
+python app.py assets watch  --parse-templates
+```
+
+> 构建这一步不是必须的，如果你的应用非常大，那么可以提前构建一下，以免影响服务器吐出页面的速度。
--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask_assets import Environment, ManageAssets
 from flask_script import Manager
-from jac_init import jac_init
+import os
 
 app = Flask(__name__)
+
+# config
+app.config.debug = os.getenv('ENV_MODE', 'DEV') != 'PROD'
+
+# flask-assets
+assets = Environment(app)
+assets.debug = app.debug
+
+# flask script
 manager = Manager(app)
-jac_init(app, manager)
+manager.add_command("assets", ManageAssets(assets))
 
 
 @app.route('/')
