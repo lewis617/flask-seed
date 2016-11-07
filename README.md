c6c6cb85b8c1f79722e8d2b21751ed6a303eed03
lewis617 <897473127@qq.com>
2016-11-07 19:44:15 +0800

    remove  flask-assets and use jac

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
 # flask-seed
 
+### 安装依赖
+
+```sh
+pip install -r reuqirements.txt
+
+npm i -g less
+```
+
+`less` 用于编译CSS（或LESS）。
+
 ### 运行方法：
 
 ```sh
 python app.py runserver -h 0.0.0.0 -p 8080
 ```
 
-> 生产环境下，需要将环境变量 ENV_MODE 设为 PROD，来禁用 DEBUG 模式。
+生产环境下，需要将环境变量 ENV_MODE 设为 PROD，来禁用 DEBUG 模式。
 
-### 构建方法
+### 离线编译
 
 ```sh
-python app.py assets build  --parse-templates
-python app.py assets clean  --parse-templates
-python app.py assets watch  --parse-templates
+python app.py jac
 ```
 
-> 构建这一步不是必须的，如果你的应用非常大，那么可以提前构建一下，以免影响服务器吐出页面的速度。
+当你将应用部署到生产环境的服务器时，需要进行离线编译，这样做有两个作用：
+
+* 离线编译后，就无需服务器响应时的临时编译了，提高了速度。另外，当你在开发过程中，觉得响应太慢时，也可以先进行离线编译，这样服务器响应就快了。
+* 当你将应用部署在多台服务器中时，就必须进行离线编译，这样可以保证每台服务的静态资源链接是相同的，从而不会导致404错误。
--- app.py
+++ app.py
@@ -1,20 +1,45 @@
 from flask import Flask, render_template
-from flask_assets import Environment, ManageAssets
 from flask_script import Manager
+from jac.contrib.flask import JAC, get_template_dirs
 import os
+import sys
+import shutil
 
 app = Flask(__name__)
 
 # config
-app.config.debug = os.getenv('ENV_MODE', 'DEV') != 'PROD'
+app.debug = os.getenv('ENV_MODE', 'DEV') != 'PROD'
 
-# flask-assets
-assets = Environment(app)
-assets.debug = app.debug
+# jac
+app.config['COMPRESSOR_DEBUG'] = app.debug
+app.config['COMPRESSOR_OUTPUT_DIR'] = '%s/static/dist' % sys.path[0]
+app.config['COMPRESSOR_STATIC_PREFIX'] = '/static/dist'
+jac = JAC(app)
 
 # flask script
 manager = Manager(app)
-manager.add_command("assets", ManageAssets(assets))
+
+
+@manager.command
+def jac():
+    env = app.jinja_env
+
+    if os.path.exists(env.compressor_output_dir):
+        print('Deleting previously compressed files in {output_dir}'
+              .format(output_dir=env.compressor_output_dir))
+        shutil.rmtree(env.compressor_output_dir)
+    else:
+        print('No previous compressed files found in {output_dir}'
+              .format(output_dir=env.compressor_output_dir))
+
+    template_dirs = [sys.path[0] + '/' + x for x in get_template_dirs(app)]
+
+    print('Compressing static assets into {output_dir}'
+          .format(output_dir=env.compressor_output_dir))
+    compressor = env.extensions['jac.extension.CompressorExtension'].compressor
+    compressor.offline_compress(env, template_dirs)
+
+    print 'Finished.'
 
 
 @app.route('/')
